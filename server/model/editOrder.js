3afd37c030f6be3bfb6f1645fa8fb15cd884b87c
DorcasEmi <dorcasemi@yahoo.com>
2018-09-26 18:54:38 -0700

    adds ui updates

--- server/model/editOrder.js
+++
@@ -1,9 +0,0 @@
-const editOrder = {
-    id: 1,
-    menu : "Fried Rice",
-    price : 1500,
-    status : "available",
-  
-};
-
-export default editOrder;
--- server/model/newOrder.js
+++
@@ -1,9 +0,0 @@
-let newOrder = {
-    id: 3,
-    menu : "Ofada Rice",
-    price : 1500,
-    status : "available",
-
-};
-
-export default newOrder;
